4ecfdfbc086dca31752a21715988fd992eaabc21
OSTWALD-Ro <139101764+OSTWALD-Ro@users.noreply.github.com>
2023-09-02 23:03:39 -0600

    Add files via upload

---
+++ Ejemplo.js
@@ -0,0 +1,4 @@
+const title1 = document.createElement('title');  title1.textContent = "title1";  document.head.appendChild(title1);
+    
+const div1 = document.createElement("div");  div1.textContent;  document.body.appendChild(div1);
+const p1 = document.createElement("p");  p1.textContent = "p1";  div1.appendChild(p1);
